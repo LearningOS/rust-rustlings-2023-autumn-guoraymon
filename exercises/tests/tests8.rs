a8d3f8f433c5b95dc42d7dbecc4dd8dc38e5e818
guoraymon <guoraymon@gmail.com>
2023-10-19 09:44:59 +0000

    day 7

--- exercises/tests/tests8.rs
+++ exercises/tests/tests8.rs
@@ -7,9 +7,9 @@
 // Execute `rustlings hint tests8` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+include!("./build.rs");
 
-fn main() {}
+// fn main() {}
 
 #[cfg(test)]
 mod tests {
